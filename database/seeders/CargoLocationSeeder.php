4d33509322da77d4ec74722825d32fcc10206467
AntonBs <reloded1995@gmail.com>
2021-10-14 16:10:28 +0300

    Add some city

--- database/seeders/CargoLocationSeeder.php
+++ database/seeders/CargoLocationSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\CargoLocation;
-use App\Models\ProductStatus;
 use Illuminate\Database\Seeder;
 
 class CargoLocationSeeder extends Seeder
@@ -17,15 +16,25 @@ class CargoLocationSeeder extends Seeder
     {
 
         CargoLocation::create(['name'=>'----','city'=>'----']);
-        CargoLocation::create(['name'=>'IEV','city'=>'KYIV']);
-        CargoLocation::create(['name'=>'HRK','city'=>'KHARKIV']);
-        CargoLocation::create(['name'=>'ODS','city'=>'ODESA']);
-        CargoLocation::create(['name'=>'LWO','city'=>'LVIV']);
-        CargoLocation::create(['name'=>'KHE','city'=>'KHERSON']);
-        CargoLocation::create(['name'=>'OZH','city'=>'ZAPORIZHZHIA']);
-        CargoLocation::create(['name'=>'UDJ','city'=>'UZHHOROD']);
+        CargoLocation::create(['name'=>'IEV(KBP)','city'=>'KYIV']);
+        CargoLocation::create(['name'=>'CKC','city'=>'CHERKASY']);
+        CargoLocation::create(['name'=>'CWC','city'=>'CHERNIVTSI']);
+        CargoLocation::create(['name'=>'CEJ','city'=>'CHERNIHIV']);
         CargoLocation::create(['name'=>'DNK','city'=>'DNIPRO']);
-        CargoLocation::create(['name'=>'IFO','city'=>'IVANO-FRANKIVSK']);
+        CargoLocation::create(['name'=>'HRK','city'=>'KHARKIV']);
+        CargoLocation::create(['name'=>'KHE','city'=>'KHERSON']);
+        CargoLocation::create(['name'=>'HMJ','city'=>'KHMELNYTSYI']);
+        CargoLocation::create(['name'=>'KGO','city'=>'KROPYVNYTSKYI']);
+        CargoLocation::create(['name'=>'LWO','city'=>'LVIV']);
+        CargoLocation::create(['name'=>'UCK','city'=>'LUTSK']);
+        CargoLocation::create(['name'=>'ODS','city'=>'ODESSA']);
+        CargoLocation::create(['name'=>'PLV','city'=>'POLTAVA']);
+        CargoLocation::create(['name'=>'RWN','city'=>'RIVNE']);
+        CargoLocation::create(['name'=>'UMY','city'=>'SUMY']);
+        CargoLocation::create(['name'=>'TNL','city'=>'TERNOPIL']);
+        CargoLocation::create(['name'=>'UDJ','city'=>'UZHHOROD']);
+        CargoLocation::create(['name'=>'VIN','city'=>'VINNYTSIA']);
+        CargoLocation::create(['name'=>'OZH','city'=>'ZAPORIZZHIA']);
         CargoLocation::create(['name'=>'ZTR','city'=>'ZHYTOMYR']);
     }
 }
