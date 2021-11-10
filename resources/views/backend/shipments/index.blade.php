ed95791ac973a164ffad08dcc78b7c3bc3578dba
Anton <antonbelouswork@gmail.com>
2021-11-10 12:32:12 +0200

    update status

--- resources/views/backend/shipments/index.blade.php
+++ resources/views/backend/shipments/index.blade.php
@@ -393,11 +393,13 @@
                                         if ($shipment->tracker->where('position','1')->count() == 1){
                                             echo '<th>'.$shipment->tracker->where('position','1')->pluck('cargolocation')->first()->name.'</th>';
                                             $location_name=!is_null($shipment->tracker->where('position','1')->where('status','Awaiting arrival')->first())?$shipment->tracker->where('position','1')->where('status','Awaiting arrival')->first()->cargolocation->name:$shipment->tracker->where('position','2')->where('status','Awaiting arrival')->first()->cargolocation->name;
-                                            echo '<th>'.$shipment->tracker->where('position','1')->where('status','Arrived')->last()->cargolocation->name.' ->'. $location_name .'</th>';                                           }
+                                            echo '<th>'.$shipment->tracker->where('position','1')->where('status','Arrived')->last()->cargolocation->name.' ->'. $location_name .'</th>';
+                                        }
                                         else{
                                             echo '<th>'.$shipment->tracker->where('position','1')->where('status','Arrived')->last()->cargolocation->name.'</th>';
                                             $location_name=!is_null($shipment->tracker->where('position','1')->where('status','Awaiting arrival')->first())?$shipment->tracker->where('position','1')->where('status','Awaiting arrival')->first()->cargolocation->name:$shipment->tracker->where('position','2')->where('status','Awaiting arrival')->first()->cargolocation->name;
-                                            echo '<th>'.$shipment->tracker->where('position','1')->where('status','Arrived')->last()->cargolocation->name.' ->'. $location_name .'</th>';                                        }
+                                            echo '<th>'.$shipment->tracker->where('position','1')->where('status','Arrived')->last()->cargolocation->name.' ->'. $location_name .'</th>';
+                                        }
                                     }
                                     elseif ($shipment->status_id == 3){
                                        echo '<th>'.$shipment->status->name.' ->'. $shipment->tracker->where('position','1')->pluck('cargolocation')->first()->name.'</th>';
@@ -405,18 +407,19 @@
                                     }
                                     elseif ($shipment->status_id == 4){
                                         $location_name=!is_null($shipment->tracker->where('position','1')->where('status','Awaiting arrival')->first())?$shipment->tracker->where('position','1')->where('status','Awaiting arrival')->first()->cargolocation->name:$shipment->tracker->where('position','2')->where('status','Awaiting arrival')->first()->cargolocation->name;
-                                        echo '<th>'.$shipment->tracker->where('position','1')->where('status','Arrived')->last()->cargolocation->name.' ->'. $location_name .'</th>';
-                                        echo '<th>'.$statuses[$shipment->status_id + 1]->name.'</th>';
-
-                                       /* if($shipment->tracker->where('position','1')->where('status','Awaiting arrival')->count() === 0)
+                                         if($shipment->tracker->where('position','1')->where('status','Awaiting arrival')->count() === 0)
                                         {
-                                           echo '<th>'.$statuses[$shipment->status_id + 1]->name.'<th>';
+                                          echo '<th>'.$shipment->tracker->where('position','1')->where('status','Arrived')->last()->cargolocation->name.' ->'. $location_name .'+POD Pending</th>';
                                         }
                                         else
                                         {
-                                            echo '<th>'.$statuses[$shipment->status_id]->name.'<th>';
-                                        } */
-
+                                           echo '<th>'.$shipment->tracker->where('position','1')->where('status','Arrived')->last()->cargolocation->name.' ->'. $location_name .'</th>';
+                                        }
+                                        echo '<th>'.$statuses[$shipment->status_id + 1]->name.'</th>';
+                                    }
+                                    elseif ($shipment->status_id == 5){
+                                        echo '<th>'. $shipment->tracker->where('position','0')->first()->cargolocation->name .'->'. $shipment->tracker->where('position','2')->first()->cargolocation->name .'+'.$shipment->status->name.'</th>';
+                                        echo '<th>'.$statuses[$shipment->status_id]->name.'</th>';
                                     }
                                     else
                                     {
@@ -445,7 +448,8 @@
                                            title="Print">
                                             <i class="las la-print"></i>
                                         </a>
-                                        <form action="{{route('admin.orders.destroy',$shipment->id)}}" class="d-inline-block" method="post">
+                                        <form action="{{route('admin.orders.destroy',$shipment->id)}}"
+                                              class="d-inline-block" method="post">
                                             @csrf
                                             @method("DELETE")
                                             <a data-toggle="tooltip" title="delete" data-id="{{$shipment->id}}"
