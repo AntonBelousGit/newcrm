051186b8d95ef0311c29ab2f7ad8a0503cbdc4d4
AntonBs <reloded1995@gmail.com>
2021-11-18 15:30:07 +0200

    Fix edit order

--- resources/views/backend/shipments/components/edit/next_point.blade.php
+++ resources/views/backend/shipments/components/edit/next_point.blade.php
@@ -15,13 +15,13 @@
     }
     elseif ($orders->status_id == 4){
         $location_name=!is_null($orders->tracker->where('position','1')->where('status','Awaiting arrival')->first())?$orders->tracker->where('position','1')->where('status','Awaiting arrival')->first()->cargolocation->name:$orders->tracker->where('position','2')->where('status','Awaiting arrival')->first()->cargolocation->name;
-        echo $statuses[$orders->status_id + 1]->name;
+        echo $status[$orders->status_id + 1]->name;
     }
     elseif ($orders->status_id == 5){
-        echo $statuses[$orders->status_id]->name;
+        echo $status[$orders->status_id]->name;
     }
     else
     {
-       echo $statuses[$orders->status_id]->name;
+       echo $status[$orders->status_id]->name;
     }
 @endphp
